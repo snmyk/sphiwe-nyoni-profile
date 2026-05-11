06ee3e178bfb2350e418158e7cd0530385923537
SphiweNyoni <165796644+SphiweNyoni@users.noreply.github.com>
2026-05-11 14:44:32 +0200

    added about page

--- sphiwe-nyoni-profile/src/app/layout/layout.component.ts
+++ sphiwe-nyoni-profile/src/app/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { HomeComponent } from '../pages/home/home.component';
+import { AboutComponent } from '../pages/about/about.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, HomeComponent, AboutComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
